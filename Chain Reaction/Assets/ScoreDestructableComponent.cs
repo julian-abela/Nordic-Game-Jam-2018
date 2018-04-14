ffaa6f2c7ec60c4bbb02a583ce585789801f4cc4
AAstrup <alexander.westi.astrup@gmail.com>
2018-04-14 13:51:34 +0200

    Score implemented

---
+++ Chain Reaction/Assets/ScoreDestructableComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDestructableComponent : MonoBehaviour
+{
+    public DestructableType type;
+    public enum DestructableType { Wall, Roof };
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Floor")
+        {
+            CanvasScore.instance.AddScore(PlayerScore.PlayerNr.P1, type);
+            Destroy(gameObject);
+        }
+    }
+}
