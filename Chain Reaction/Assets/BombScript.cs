6e520fc6bb52c2f54a40408a007660a528ed8008
Kasper Christian Hansen <kchh@itu.dk>
2018-04-14 13:33:59 +0200

    Bombs and shit

---
+++ Chain Reaction/Assets/BombScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour {
+
+    private SphereCollider sc;
+    private Rigidbody rb;
+
+    public float explosionForce = 5f;
+    public float maxSize;
+    public float timer;
+
+	// Use this for initialization
+	void Start () {
+        sc = GetComponent<SphereCollider>();
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer <= 0f)
+        {
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, maxSize);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rigidBody = hit.GetComponent<Rigidbody>();
+
+                if (rigidBody != null)
+                    rigidBody.AddExplosionForce(explosionForce, explosionPos, maxSize);
+            }
+        }
+        else
+            timer -= Time.deltaTime;
+	}
+}
---
+++ Chain Reaction/Assets/PlaceBomb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBomb : MonoBehaviour {
+
+    public Camera camera;
+    public GameObject bomb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyUp(KeyCode.Mouse0)) {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+                Instantiate(bomb,hit.point,objectHit.rotation);
+
+            }
+        }
+	}
+}
