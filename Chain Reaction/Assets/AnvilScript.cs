1ad10f9d6b060fc99dd1353c05de15b430e19aba
AAstrup <alexander.westi.astrup@gmail.com>
2018-04-14 21:27:23 +0200

    prefab

---
+++ Chain Reaction/Assets/AnvilScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnvilScript : MonoBehaviour {
+    private float fireSpeed = 3f;
+    private Rigidbody body;
+    private Vector3 leftEnd;
+    private Vector3 rightEnd;
+    private float range = 10f;
+    private float lerpValue = .5f;
+    private float inputSensitivity;
+
+    private void Start()
+    {
+        inputSensitivity = 1f / range;
+        body = GetComponent<Rigidbody>();
+        transform.position += new Vector3(0,2f);
+        leftEnd = transform.position + transform.right * range;
+        rightEnd = transform.position + transform.right * range * -1f;
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.useGravity = true;
+            body.AddForce(Vector3.down * fireSpeed, ForceMode.Force);
+            Destroy(this);
+        }
+        else
+        {
+            lerpValue += Input.GetAxisRaw("Horizontal") * inputSensitivity;
+            lerpValue = Mathf.Max(0, Mathf.Min(lerpValue, 1));
+            transform.position = Vector3.Lerp(rightEnd, leftEnd, lerpValue);
+        }
+	}
+}
