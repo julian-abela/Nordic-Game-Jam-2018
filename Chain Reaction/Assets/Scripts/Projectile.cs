349fe4805969905253ae5b8310aa01f35de77514
Julian Anthony Abela <juab@itu.dk>
2018-04-14 11:45:14 +0200

    Updated Projectile to be fully controllable

--- Chain Reaction/Assets/Scripts/Projectile.cs
+++ Chain Reaction/Assets/Scripts/Projectile.cs
@@ -10,6 +10,31 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position - transform.right * step;
+        } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + transform.right * step;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position - transform.up * step;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + transform.up * step;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            transform.position = transform.position + transform.forward * step;
+        } else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.position = transform.position - transform.forward * step;
+        }
     }
 }
